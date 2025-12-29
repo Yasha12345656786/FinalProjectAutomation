5fd55f10b0ea3c3b5a0a733f54c143a5b3dd53e8
yasha <jacob210usu@gmail.com>
2025-12-29 09:34:05 +0200

    Twelveth commit Friends and Favorites added

--- tests/test_Search.py
+++ tests/test_Search.py
@@ -9,9 +9,9 @@ from pages.Search import Search
 @allure.suite("Search")
 def test_searchGame(set_up):
     driver = set_up
-    l = Login(driver)
+    #l = Login(driver)
     s = Search(driver)
-    l.Login("hiwigi9610@roastic.com", "wttew12445")
+    #l.Login("hiwigi9610@roastic.com", "wttew12445")
     rtn = s.searchForGame("Mario")
     assert rtn == "Mario"
 
